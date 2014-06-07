c7d9d78da78ab74e57116d3a15472627ee01827f
Aleksey Kurepin <lesha@kurepin.com>
2014-06-07 17:11:40 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![KEEP CALM and ENJOY THE WEDDING](public/images/keep-calm-and-enjoy-the-wedding-64.png)
---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+require 'mina/git'
+
+set :domain, '37.139.12.234'
+set :user, 'foxweb'
+set :deploy_to, '/home/foxweb/www/wedding.kurepin.com'
+set :repository, 'git@github.com:foxweb/wedding.git'
+set :branch, 'master'
+set :keep_releases, 5
+
+task :setup => :environment do
+  # nothing
+end
+
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      # nothing
+    end
+  end
+end
